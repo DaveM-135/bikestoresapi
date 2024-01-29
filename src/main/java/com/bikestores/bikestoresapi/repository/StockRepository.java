caf1cf864695b9a9e8b271e0930f5397ce5b422f
DaveM-135 <67597482+DaveM-135@users.noreply.github.com>
2024-01-29 15:24:41 -0600

    updates
    Actualización api bikestores

--- src/main/java/com/bikestores/bikestoresapi/repository/StockRepository.java
+++ src/main/java/com/bikestores/bikestoresapi/repository/StockRepository.java
@@ -3,8 +3,7 @@ package com.bikestores.bikestoresapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.bikestores.bikestoresapi.model.Stock;
-import com.bikestores.bikestoresapi.model.Store;
 
-public interface StockRepository extends JpaRepository<Stock, Store> {
+public interface StockRepository extends JpaRepository<Stock, Long> {
     
 }
